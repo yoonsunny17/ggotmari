b2bbdf85692a0674df7005e165d5a4adc2d44d54
yoonsunny17 <jyoonsun0217@gmail.com>
2022-09-13 13:43:06 +0900

    [FE] settings: Update tailwind color palette

--- frontend/ggotmari/tailwind.config.js
+++ frontend/ggotmari/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        font1: "#333333",
+        font2: "#84898C",
+        font3: "#F3F3F3",
+        main: "#6E85B7",
+        sub1: "#B2C8DF",
+        sub2: "#709FB0",
+        sub3: "#FFD365",
+        extra1: "#57837B",
+        extra2: "#A0C1B8",
+        extra3: "#FFE194",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
