33cf761658bfb52354e56ed3b0c703ae980aac47
YunYoung <jeongyy98>
2022-09-14 14:05:33 +0900

    [BE] feat : Add comment entity

---
+++ backend/src/main/java/com/ssafy/db/entity/Comment.java
@@ -0,0 +1,35 @@
+package com.ssafy.db.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id", nullable = false, updatable = false, columnDefinition = "BIGINT")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDate date;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
