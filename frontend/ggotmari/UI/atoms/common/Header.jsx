19869a2269e5fe82e663f9665c7e054357cb9a45
yoonsunny17 <jyoonsun0217@gmail.com>
2022-09-14 15:50:10 +0900

    [FE] feat : Update atom component - Header

--- frontend/ggotmari/UI/atoms/common/Header.jsx
+++ frontend/ggotmari/UI/atoms/common/Header.jsx
@@ -1,9 +1,7 @@
 export default function Header({ text, type, onClick }) {
   return (
-    <div>
-      <div className="flex justify-center font-maru text-main text-2xl mt-3">
-        {text}
-      </div>
+    <div className="flex justify-center font-maru text-main text-2xl mt-3">
+      {text}
     </div>
   );
 }
