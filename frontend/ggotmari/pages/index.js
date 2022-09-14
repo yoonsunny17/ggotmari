d7d31a4acc816d2d45fc70c82e7905cff4b13785
yoonsunny17 <jyoonsun0217@gmail.com>
2022-09-14 15:51:16 +0900

    [FE] feat: Add main page

--- frontend/ggotmari/pages/index.js
+++ frontend/ggotmari/pages/index.js
@@ -1,3 +1,15 @@
+import Header from "../UI/atoms/common/Header";
+import SearchBar from "../UI/molecules/common/SearchBar";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      {/* <div className="flex justify-center font-maru text-main text-2xl mt-3">
+        꽃마리
+      </div> */}
+      <Header text={"꽃마리"} />
+      <hr />
+      <SearchBar placeholder={"꽃 찾기"} />
+    </div>
+  );
 }
