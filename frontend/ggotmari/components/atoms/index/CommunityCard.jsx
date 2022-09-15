b39b4cb347e69f92124e8ce76ba1e345183d15f5
yoonsun <jyoonsun0217@gmail.com>
2022-09-15 16:34:40 +0900

    [FE] feat : Add CommunityCard

---
+++ frontend/ggotmari/components/atoms/index/CommunityCard.jsx
@@ -0,0 +1,28 @@
+import { IoMdHeart } from "react-icons/io";
+
+function CommunityCard({ info: { username, imgUrl, likeNumbs } }) {
+  return (
+    <div className="">
+      <div className="cursor-pointer rounded-2xl aspect-square shadow overflow-hidden relative">
+        <img
+          src={imgUrl}
+          alt="communityCard"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="flex justify-between mt-1 text-xs">
+        <p className="text-font2">@ {username}</p>
+        <p className="flex">
+          <IoMdHeart size={15} color="red" />{" "}
+          {likeNumbs >= 100 ? (
+            "99+"
+          ) : (
+            <span className="text-font1">{likeNumbs}</span>
+          )}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CommunityCard;
