d19656245bb310ffb973f33b70c1ea416362238d
YunYoung <jeongyy98>
2022-09-16 13:57:38 +0900

    [BE] feat : Add /community/flower/search/{searchText} - get

--- backend/src/main/java/com/ssafy/api/controller/FlowerController.java
+++ backend/src/main/java/com/ssafy/api/controller/FlowerController.java
@@ -2,8 +2,10 @@ package com.ssafy.api.controller;
 
 import com.ssafy.api.response.DailyFlowerGetRes;
 import com.ssafy.api.response.SubjectGetRes;
+import com.ssafy.api.response.searchFlowerGetRes;
 import com.ssafy.api.service.FlowerService;
 import com.ssafy.db.entity.DailyFlower;
+import com.ssafy.db.entity.Kind;
 import com.ssafy.db.entity.Subject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,8 +14,11 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api(value = "꽃 api", tags = {"Flower"})
 @RestController
@@ -40,8 +45,22 @@ public class FlowerController {
         }
     }
 
-    //TODO : 꽃 검색
+    @GetMapping("/search/{searchText}")
+    @ApiOperation(value = "꽃 검색", notes = "searchText에 적합한 꽃을 반환한다.")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "꽃 검색 성공"),
+            @ApiResponse(code = 500, message = "꽃 검색 실패")
+    })
+    public ResponseEntity<? extends searchFlowerGetRes> searchFlowers(@PathVariable("searchText") String searchText){
 
+        List<Kind> flowers = flowerService.findFlowers(searchText);
+
+        if(flowers == null){
+            return ResponseEntity.status(403).body(searchFlowerGetRes.of(403, "조회실패.", null));
+        }else{
+            return ResponseEntity.status(201).body(searchFlowerGetRes.of(201, "정상적으로 조회되었습니다.", flowers));
+        }
+    }
 
     //TODO : 품목 상세 페이지 조회
 
--- backend/src/main/java/com/ssafy/api/service/FlowerService.java
+++ backend/src/main/java/com/ssafy/api/service/FlowerService.java
@@ -1,15 +1,18 @@
 package com.ssafy.api.service;
 
 import com.ssafy.db.entity.DailyFlower;
+import com.ssafy.db.entity.Kind;
 import com.ssafy.db.entity.Subject;
 import com.ssafy.db.repository.DailyFlowerRespository;
+import com.ssafy.db.repository.KindRepository;
+import com.ssafy.db.repository.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,8 +20,29 @@ public class FlowerService {
 
     @Autowired
     DailyFlowerRespository dailyFlowerRespository;
+    @Autowired
+    KindRepository kindRepository;
+    @Autowired
+    SubjectRepository subjectRepository;
 
     public DailyFlower getDailyFlower() {
         return dailyFlowerRespository.findDailyFlowerByFlowerDate(LocalDate.now().getMonthValue() + "-" + LocalDate.now().getDayOfMonth());
+    }
+
+    public List<Kind> findFlowers(String searchText){
+        //품종 검색
+        List<Kind> kinds = kindRepository.findAllByKindNameContains(searchText);
+
+        //품목 검색
+        List<Subject> subjects = subjectRepository.findAllBySubjectNameContains(searchText);
+        for(Subject subject : subjects){
+            List<Kind> tmp = kindRepository.findAllBySubject(subject);
+            kinds.addAll(tmp);
+        }
+
+        //중복 제거
+        List<Kind> flowers = kinds.stream().distinct().collect(Collectors.toList());
+
+        return flowers;
     }
 }
