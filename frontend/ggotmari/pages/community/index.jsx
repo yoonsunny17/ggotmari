716ac7a777562386619dfea6e2d8b097b3f3e06d
yoonsun <jyoonsun0217@gmail.com>
2022-09-19 16:16:30 +0900

    [FE] fix : Update flower daily page

--- frontend/ggotmari/pages/community/index.jsx
+++ frontend/ggotmari/pages/community/index.jsx
@@ -38,6 +38,136 @@ export default function Community() {
       commentCount: 134,
       likeCount: 178,
     },
+    {
+      articleId: 3,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+      userId: "sangchuman",
+      articleTitle: "여자친구한테 칭찬 받았어요",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["거베라", "장미"],
+      commentCount: 5,
+      likeCount: 15,
+    },
+    {
+      articleId: 4,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/005a177b-e5f0-403d-bf7b-20da2560d54a_%EA%BC%AC%EB%B6%80%EA%B8%B0.png",
+      userId: "hotbubbletea",
+      articleTitle: "자기야 결혼하자",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["국화"],
+      commentCount: 134,
+      likeCount: 178,
+    },
+    {
+      articleId: 5,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+      userId: "sangchuman",
+      articleTitle: "여자친구한테 칭찬 받았어요",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["거베라", "장미"],
+      commentCount: 5,
+      likeCount: 15,
+    },
+    {
+      articleId: 6,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/005a177b-e5f0-403d-bf7b-20da2560d54a_%EA%BC%AC%EB%B6%80%EA%B8%B0.png",
+      userId: "hotbubbletea",
+      articleTitle: "자기야 결혼하자",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["국화"],
+      commentCount: 134,
+      likeCount: 178,
+    },
+    {
+      articleId: 7,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+      userId: "sangchuman",
+      articleTitle: "여자친구한테 칭찬 받았어요",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["거베라", "장미"],
+      commentCount: 5,
+      likeCount: 15,
+    },
+    {
+      articleId: 8,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/005a177b-e5f0-403d-bf7b-20da2560d54a_%EA%BC%AC%EB%B6%80%EA%B8%B0.png",
+      userId: "hotbubbletea",
+      articleTitle: "자기야 결혼하자",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["국화"],
+      commentCount: 134,
+      likeCount: 178,
+    },
+    {
+      articleId: 9,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+      userId: "sangchuman",
+      articleTitle: "여자친구한테 칭찬 받았어요",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["거베라", "장미"],
+      commentCount: 5,
+      likeCount: 15,
+    },
+    {
+      articleId: 10,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/005a177b-e5f0-403d-bf7b-20da2560d54a_%EA%BC%AC%EB%B6%80%EA%B8%B0.png",
+      userId: "hotbubbletea",
+      articleTitle: "자기야 결혼하자",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["국화"],
+      commentCount: 134,
+      likeCount: 178,
+    },
+    {
+      articleId: 11,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+      userId: "sangchuman",
+      articleTitle: "여자친구한테 칭찬 받았어요",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["거베라", "장미"],
+      commentCount: 5,
+      likeCount: 15,
+    },
+    {
+      articleId: 12,
+      articleImage:
+        "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/005a177b-e5f0-403d-bf7b-20da2560d54a_%EA%BC%AC%EB%B6%80%EA%B8%B0.png",
+      userId: "hotbubbletea",
+      articleTitle: "자기야 결혼하자",
+      articleContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat vel quam ...",
+      articleDate: "2022.09.07 14:04",
+      tags: ["국화"],
+      commentCount: 134,
+      likeCount: 178,
+    },
   ];
 
   return (
--- frontend/ggotmari/pages/flower/daily.jsx
+++ frontend/ggotmari/pages/flower/daily.jsx
@@ -1,5 +1,25 @@
+import Header from "../../components/atoms/common/Header";
+
+// TODO: 날짜에 맞는 꽃 정보 렌더링
 function DailyFlower() {
-  return <div>daily flower recommend</div>;
+  return (
+    <div className="flex flex-col">
+      <div>
+        <Header text={"오늘의 꽃"} />
+      </div>
+      <img
+        className="aspect-square object-cover"
+        src="https://images.unsplash.com/photo-1527061011665-3652c757a4d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80"
+        alt=""
+      />
+    </div>
+  );
 }
 
+const DailyFlowers = [
+  {
+    imgUrl:
+      "https://images.unsplash.com/photo-1527061011665-3652c757a4d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
+  },
+];
 export default DailyFlower;
