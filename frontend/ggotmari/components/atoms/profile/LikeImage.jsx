35775b5a1fa94d5d306217b9213e37fd55f71a8e
unknown <dytjd297@gmail.com>
2022-09-19 13:54:39 +0900

    [FE] feat : Temp commit

--- frontend/ggotmari/components/atoms/profile/LikeImage.jsx
+++ frontend/ggotmari/components/atoms/profile/LikeImage.jsx
@@ -3,7 +3,7 @@ import { IoMdHeart } from "react-icons/io";
 function LikeImage({ articleId, articleImage, userName, likes }) {
   console.log({ articleId, articleImage, userName, likes });
   return (
-    <div className="aspect-square col-span-1 p-1.5">
+    <div className="aspect-square col-span-1 p-1.5 flex flex-col">
       <div className="article-img cursor-pointer w-full h-full grid justify-items-end rounded-md bg-[url('https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg')] bg-cover">
         <div className="heart-box flex items-end p-1">
           <span className="flex items-center text-xs p-0.5 text-white bg-[#84898C] rounded-md">
@@ -12,6 +12,7 @@ function LikeImage({ articleId, articleImage, userName, likes }) {
           </span>
         </div>
       </div>
+
       <span className="username text-font2 font-sansultralight text-[0.5rem]">
         @{userName}
       </span>
