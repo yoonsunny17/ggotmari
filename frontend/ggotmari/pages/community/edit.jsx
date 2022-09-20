017f2d381e51ebd54013fe83b2ef1c1787737653
Youngjoon Park <junpark364@gmail.com>
2022-09-21 01:22:15 +0900

    [FE] feat : Update article edit page

--- frontend/ggotmari/pages/community/edit.jsx
+++ frontend/ggotmari/pages/community/edit.jsx
@@ -1,26 +1,32 @@
 import { useRouter } from "next/router";
-import { GrGallery, GrPowerReset } from "react-icons/gr";
+import { useState } from "react";
+import { IoCameraOutline, IoRefreshOutline } from "react-icons/io5";
+import FlowerTag from "../../components/atoms/common/FlowerTag";
 
 function EditArticle() {
   const router = useRouter();
+  const [flowerTags, setFlowerTags] = useState([
+    "장미",
+    "거베라",
+    "코스모스",
+    "아크라시아",
+  ]);
 
   return (
     <div className="flex flex-col items-center">
       <div className="w-full aspect-square bg-main"></div>
-      <div className="flex flex-row justify-center">
-        <div className="inline text-font2">
-          <GrGallery className="inline text-font2" />
-          사진 첨부
+      <div className="flex flex-row space-x-3 justify-center my-3">
+        <div className="inline text-font2 cursor-pointer">
+          <IoCameraOutline className="inline" /> 사진 첨부
         </div>
         <p> | </p>
-        <div className="inline text-font2">
-          <GrPowerReset className="inline text-font2" />
-          초기화
+        <div className="inline text-font2 cursor-pointer">
+          <IoRefreshOutline className="inline" /> 초기화
         </div>
       </div>
       <div className="w-full p-3">
-        <form className="flex flex-col w-full space-y-2 font-sans text-font2">
-          <label for="articleTitle" className="pl-2 text-sm">
+        <form className="flex flex-col w-full space-y-4 font-sans text-font2">
+          <label htmlFor="articleTitle" className="pl-2 text-sm">
             글 제목
           </label>
           <input
@@ -29,21 +35,34 @@ function EditArticle() {
             className="shadow-md w-full text-sm focus:outline-none px-3 py-2"
             placeholder="제목을 입력하세요"
           />
-          <label for="articleTitle" className="pl-2 text-sm">
+          <label htmlFor="flowerTags" className="pl-2 text-sm">
             꽃 태그
           </label>
-          <input
-            type="text"
-            className="shadow-md w-full text-lg focus:outline-none p-3"
-          />
-          <label for="articleTitle" className="pl-2 text-sm">
+          <div className="w-full shadow-md">
+            <div>
+              <div className="flex flex-row flex-wrap px-5 py-3">
+                {flowerTags?.map((tag) => (
+                  <FlowerTag flowerName={tag} key={tag} isRemovable={true} />
+                ))}
+              </div>
+              <hr />
+            </div>
+
+            <input
+              type="text"
+              className="w-full text-sm focus:outline-none p-3"
+              placeholder="꽃을 검색하세요"
+            />
+          </div>
+          <label htmlFor="articleContent" className="pl-2 text-sm">
             내용
           </label>
-          <input
-            type="text"
-            className="shadow-md w-full text-sm focus:outline-none p-3 h-40"
+          <textarea
+            id="articleContent"
+            rows="5"
+            className="shadow-md w-full text-sm focus:outline-none p-3"
             placeholder="내용을 입력하세요"
-          />
+          ></textarea>
           <input type="submit" />
         </form>
       </div>
