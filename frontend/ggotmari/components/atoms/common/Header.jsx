22c0237cce8234ed77a61d809f8c8c78f3895a8b
yoonsunny17 <jyoonsun0217@gmail.com>
2022-09-20 14:34:30 +0900

    [FE] fix : Mod font style

--- frontend/ggotmari/components/atoms/common/Header.jsx
+++ frontend/ggotmari/components/atoms/common/Header.jsx
@@ -1,6 +1,6 @@
 function Header({ text, type, onClick }) {
   return (
-    <div className="flex justify-center font-maru text-main text-xl font-semibold my-5">
+    <div className="flex justify-center font-gangwon text-main text-2xl font-semibold mt-5 mb-4">
       {text}
     </div>
   );
