69cfb9181dd1fce74c30f8b1ba0725d9170820d8
cloudsea1106 <cloudsea1106@gmail.com>
2022-09-21 16:15:11 +0900

    [BE] settings : Set wsgi

--- backend_django/ggotmari/urls.py
+++ backend_django/ggotmari/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/flower', include('flowers.urls')),
+    path('data/flower', include('flowers.urls')),
 ]
