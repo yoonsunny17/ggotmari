7f047478816f64785cf4b5f2d56d158dea873ff8
yoonsunny17 <jyoonsun0217@gmail.com>
2022-09-21 18:09:58 +0900

    [FE] feat : Add RelatedPosts

--- frontend/ggotmari/pages/flower/[kindId].jsx
+++ frontend/ggotmari/pages/flower/[kindId].jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import RelatedPosts from "../../components/molecules/flower/RelatedPosts";
 import SimilarFlowers from "../../components/molecules/flower/SimilarFlowers";
 
 function FlowerDetail(params) {
@@ -94,8 +95,16 @@ function FlowerDetail(params) {
         </div>
 
         {/* 연관 게시물 보기 */}
-        <div className="font-gangwon text-lg py-4">
-          {flowerInfo.flower.subjectName}를 담은 이야기
+        <div>
+          <div className="font-gangwon text-lg py-4">
+            {flowerInfo.flower.subjectName}를 담은 이야기
+          </div>
+
+          <div className="grid grid-cols-3 gap-x-3">
+            {relatedPostArr.map((info, idx) => {
+              return <RelatedPosts info={info} key={idx} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
@@ -220,6 +229,93 @@ const similarFlowerArr = [
     kindName: "미니",
     imgUrl:
       "https://images.unsplash.com/photo-1588140096821-99ffc14c4bc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+];
+
+const relatedPostArr = [
+  {
+    articleId: 1111,
+    articleImage:
+      "https://images.unsplash.com/photo-1496483648148-47c686dc86a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+    likeCount: 23,
+    username: "yoonsunny",
+  },
+  {
+    articleId: 1112,
+    articleImage:
+      "https://images.unsplash.com/photo-1561181286-d3fee7d55364?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    likeCount: 288,
+    username: "flower_mania",
+  },
+  {
+    articleId: 1113,
+    articleImage:
+      "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+    likeCount: 59,
+    username: "sangchuman",
+  },
+  {
+    articleId: 1114,
+    articleImage:
+      "https://images.unsplash.com/photo-1510894399130-57dfa8dcc45d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80",
+    likeCount: 100,
+    username: "ggotmari_jjang",
+  },
+  {
+    articleId: 1111,
+    articleImage:
+      "https://images.unsplash.com/photo-1496483648148-47c686dc86a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+    likeCount: 23,
+    username: "yoonsunny",
+  },
+  {
+    articleId: 1112,
+    articleImage:
+      "https://images.unsplash.com/photo-1561181286-d3fee7d55364?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    likeCount: 288,
+    username: "flower_mania",
+  },
+  {
+    articleId: 1113,
+    articleImage:
+      "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+    likeCount: 59,
+    username: "sangchuman",
+  },
+  {
+    articleId: 1114,
+    articleImage:
+      "https://images.unsplash.com/photo-1510894399130-57dfa8dcc45d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80",
+    likeCount: 100,
+    username: "ggotmari_jjang",
+  },
+  {
+    articleId: 1111,
+    articleImage:
+      "https://images.unsplash.com/photo-1496483648148-47c686dc86a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+    likeCount: 23,
+    username: "yoonsunny",
+  },
+  {
+    articleId: 1112,
+    articleImage:
+      "https://images.unsplash.com/photo-1561181286-d3fee7d55364?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    likeCount: 288,
+    username: "flower_mania",
+  },
+  {
+    articleId: 1113,
+    articleImage:
+      "https://parsley-bucket.s3.ap-northeast-2.amazonaws.com/00bf16c0-06d3-4a01-82f0-9f882d3333f5_%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5%EB%B0%A5.jpg",
+    likeCount: 59,
+    username: "sangchuman",
+  },
+  {
+    articleId: 1114,
+    articleImage:
+      "https://images.unsplash.com/photo-1510894399130-57dfa8dcc45d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80",
+    likeCount: 100,
+    username: "ggotmari_jjang",
   },
 ];
 
