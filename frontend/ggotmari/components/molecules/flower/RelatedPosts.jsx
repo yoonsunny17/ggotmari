66efc3eddecbc8ecd22e5cdf6b53ff3379e593d7
yoonsun <jyoonsun0217@gmail.com>
2022-09-22 11:09:40 +0900

    [FE] fix : Mod related post component style

--- frontend/ggotmari/components/molecules/flower/RelatedPosts.jsx
+++ frontend/ggotmari/components/molecules/flower/RelatedPosts.jsx
@@ -16,11 +16,13 @@ function RelatedPosts({
           className="w-full h-full aspect-square object-cover rounded-md overflow-hidden brightness-98"
         />
         <div className="-translate-y-6">
-          <div className="flex items-center bg-font4/40 w-fit px-1.5 py-0.5 rounded-md">
-            <IoMdHeart size={14} color="red" />{" "}
-            <p className="text-[11px] pl-0.5 text-font3">
-              {likeCount >= 100 ? "+99" : `${likeCount}`}
-            </p>
+          <div className="flex justify-end px-1">
+            <div className="flex items-center bg-font4/40 w-fit px-1.5 py-0.5 rounded-md">
+              <IoMdHeart size={14} color="red" />{" "}
+              <p className="text-[11px] pl-0.5 text-font3">
+                {likeCount >= 100 ? "+99" : `${likeCount}`}
+              </p>
+            </div>
           </div>
           <div className="text-[11px] text-font2 pt-1">@{username}</div>
         </div>
