a0f9c22336817bb80bbf392cb13dbb66362d48e7
yoonsunny17 <jyoonsun0217@gmail.com>
2022-09-23 18:05:27 +0900

    [FE] feat : Add waiting page

---
+++ frontend/ggotmari/pages/recommend/waiting.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/atoms/common/Header";
+
+function WaitingResult() {
+  return (
+    <div>
+      <Header text={"꽃에 담은 편지"} />
+      <div className="font-gangwon">편지를 읽고 있어요...!</div>
+    </div>
+  );
+}
+
+export default WaitingResult;
