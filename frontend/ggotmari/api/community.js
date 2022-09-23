b979f2874c26a4d173ec1a0bea4e83c35c828d71
Youngjoon Park <junpark364@gmail.com>
2022-09-23 10:14:43 +0900

    [FE] fix : Mod api

--- frontend/ggotmari/api/community.js
+++ frontend/ggotmari/api/community.js
@@ -5,9 +5,9 @@ const api = apiInstance();
 async function getFlowerKind(success, fail) {
   await api
     .get(`/api/community/article`, {
-      //   headers: {
-      //     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      //   },
+      // headers: {
+      //   Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      // },
     })
     .then(success)
     .catch(fail);
