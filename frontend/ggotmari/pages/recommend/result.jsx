06df6162c05fd8da2a07a945dc3bf5a8daf7e7c0
yoonsunny17 <jyoonsun0217@gmail.com>
2022-09-26 00:11:37 +0900

    [FE] fix : Update recommend/result

--- frontend/ggotmari/pages/recommend/result.jsx
+++ frontend/ggotmari/pages/recommend/result.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import Header from "../../components/atoms/common/Header";
 
 function LetterResult() {
   return (
     <div>
+      <Header text={"꽃에 담은 편지"} />
+      <img src="" alt="" />
       <div>recommend result page</div>
     </div>
   );
