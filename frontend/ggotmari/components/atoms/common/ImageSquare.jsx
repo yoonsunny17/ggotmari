bedfa8bcd7f1307b3b2541acfd425420f7f49ae0
Youngjoon Park <junpark364@gmail.com>
2022-09-28 15:28:02 +0900

    [FE] fix : Mod article item layout

--- frontend/ggotmari/components/atoms/common/ImageSquare.jsx
+++ frontend/ggotmari/components/atoms/common/ImageSquare.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function ImageSquare({ imageSrc }) {
   return (
-    <div className="w-full aspect-square opbj bg-main overflow-hidden rounded-lg relative">
+    <div className="w-full aspect-square bg-main overflow-hidden rounded-lg relative">
       {/* <img className="w-full h-full object-cover" src={imageSrc} /> */}
       <Image src={imageSrc} layout="fill" className="object-cover" />
     </div>
