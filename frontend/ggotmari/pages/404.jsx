1abb790e915bf754d9dcd6b1c20b6b35f00765ed
MoonysKR <dytjd297@gmail.com>
2022-10-04 14:33:02 +0900

    [FE] feat : Add head and Mod button & a tag attributes

--- frontend/ggotmari/pages/404.jsx
+++ frontend/ggotmari/pages/404.jsx
@@ -1,11 +1,17 @@
 import { useRouter } from "next/router";
 import Flower from "../assets/404/404flower.jpg";
 import Image from "next/image";
+import Head from "next/head";
 
 function Page404() {
   const router = useRouter();
   return (
     <div className="404 w-screen font-maru">
+      <Head>
+        <title>404 | GGOTMARI</title>
+        <meta property="og:title" content="404 Not Found" key="404" />
+        <meta name="description" content="Page not found." />
+      </Head>
       <div className="404 flex justify-center mt-24 text-6xl text-main">
         <span>404</span>
       </div>
@@ -23,6 +29,7 @@ function Page404() {
       </div>
       <div className="404-btn flex justify-center mt-14 cursor-pointer text-md text-white h-10">
         <button
+          aria-label="home"
           className="w-3/5 bg-sub1 rounded-md hover:bg-main"
           onClick={() => router.push("/")}
         >
